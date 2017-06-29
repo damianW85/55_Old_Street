aaec92d296b7370bcfcc095e5826113538572c95
Damian Whyte <damianwhyte@Inter209-2.local>
2017-06-29 16:25:35 +0100

    mobile menu functionality added

--- javaScript/app.js
+++ javaScript/app.js
@@ -33,3 +33,38 @@ setInterval(function() {
   var spritePosition = Math.floor((Math.random() * 20) + 1) * 124.68;
   dynamicGlyf.style.backgroundPositionX = '-' + spritePosition+ 'px';
 }, 2000)
+
+var burger = document.getElementById('main-burger'), 
+nav = document.getElementById('mainNav');
+
+function navControl() {
+
+  if(burger.classList.contains('burger--active')) {
+
+    nav.classList.remove('nav_open');
+    burger.classList.remove('burger--active');
+    document.body.style.position = 'relative';
+  } 
+  else {
+    burger.classList.add('burger--active');
+    nav.classList.add('nav_open');
+    document.body.style.position = 'fixed';
+  }
+}
+
+burger.addEventListener('click', navControl);
+
+var navLinks = document.querySelectorAll('.nav_link');
+
+navLinks.forEach(function(navLink) {
+
+  navLink.addEventListener('click', function() {
+
+    if(burger.classList.contains('burger--active')) {
+
+      nav.classList.remove('nav_open');
+      burger.classList.remove('burger--active');
+      document.body.style.position = 'relative';
+    } 
+  });
+});
